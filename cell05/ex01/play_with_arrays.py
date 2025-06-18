7282a790e284c4f7cd3d201012b18404426847bf
tpoosama <thachchaphrphusman@gmail.com>
2025-06-18 05:42:57 +0000

    ex01

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+original = [2, 8, 9, 48, 8, 22, -12, 2]
+new = []
+
+for n in original:
+    new.append(n + 2)
+
+print("Original array:", original)
+print("New array:", new)
